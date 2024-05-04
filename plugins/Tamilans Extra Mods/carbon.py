b6fbb52b09bfd4d573b4f5499dbc5ff59fdcc7d6
tkxleechbo <165795555+tkxleechbo@users.noreply.github.com>
2024-05-04 20:23:03 +0530

    Update carbon.py

--- plugins/Tamilans Extra Mods/carbon.py
+++ plugins/Tamilans Extra Mods/carbon.py
@@ -42,8 +42,8 @@ async def carbon_func(_, message):
     await m.edit("ᴜᴘʟᴏᴀᴅɪɴɢ..")
     await message.reply_photo(
         photo=carbon,
-        caption="**CᴏᴅᴇᴅᴇD Bʏ @Tamilan_BotsZ**",
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝚄𝚂", url="https://t.me/mkn_bots_updates")]]),                   
+        caption="**CᴏᴅᴇᴅᴇD Bʏ @Tamil_MOB_HD**",
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝚄𝚂", url="https://t.me/Tamil_MOB_HD")]]),                   
     )
     await m.delete()
     carbon.close()
